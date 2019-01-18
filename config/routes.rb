e194aaccdbe0e4f015023b1eb932beb494825cc9
Sunil kumar Pissaye <sunil.pissaye@gmail.com>
2019-01-18 18:36:51 +0530

    * load resque_web only in development env

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
-require "resque_web"
-
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -9,6 +7,7 @@ Rails.application.routes.draw do
 end
 
 if Rails.env.development?
+  require "resque_web"
   Rails.application.routes.draw do
     mount ResqueWeb::Engine => "/resque_web"
   end
