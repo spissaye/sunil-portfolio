ff2ef38fc575ef5e58889e4a61a31e38db18a604
Sunil kumar Pissaye <sunil.pissaye@gmail.com>
2019-01-19 20:38:51 +0530

    * fix flaticon font url paths * set sendmail config in production * add elastic beanstalk files to gitignore *

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,12 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sunil-portfolio_#{Rails.env}"
 
+  # Action mail configuration.
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
